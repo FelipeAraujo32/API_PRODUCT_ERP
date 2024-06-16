7e7369638e4a5921a14af7e8b26f77aa2e695e31
FelipeAraujo32 <felipecafsx@gmail.com>
2024-06-16 18:16:38 -0300

    Unit test implementations

--- product_api/src/test/java/com/product/product_api/service/ProductServiceTest.java
+++ product_api/src/test/java/com/product/product_api/service/ProductServiceTest.java
@@ -1,14 +1,22 @@
 package com.product.product_api.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +29,7 @@ import org.mockito.MockitoAnnotations;
 import com.product.product_api.entity.Product;
 import com.product.product_api.repository.ProductRepository;
 import com.product.product_api.service.business_exception.BusinessException;
+import com.product.product_api.service.business_exception.NotFoundException;
 
 public class ProductServiceTest {
 
@@ -33,11 +42,13 @@ public class ProductServiceTest {
     @Mock
     private ValidationDataService validationService;
 
-    Product product = new Product("Felipe", "Araujo", 10, 50);
+    Product product;
 
     @BeforeEach
-    void setup() {
+    void setUp() {
         MockitoAnnotations.openMocks(this);
+        product = new Product("Felipe", "Araujo", 10, 50);
+        product.setUuid(UUID.randomUUID());
     }
 
     @Test
@@ -59,7 +70,7 @@ public class ProductServiceTest {
     }
 
     @Test
-    @DisplayName("Must return a business exception if validation returns incorrectl")
+    @DisplayName("Must return a business exception if validation returns incorrect")
     void testCreateProductCase2() {
 
         doThrow(new BusinessException(toString())).when(validationService).validation(product);
@@ -91,13 +102,65 @@ public class ProductServiceTest {
     }
 
     @Test
-    void testDeleteProduct() {
+    @DisplayName("Success case when deleting the product")
+    void testDeleteProductCase1() {
 
+        when(repository.findById(product.getUuid())).thenReturn(Optional.of(product));
+
+        service.deleteProduct(product.getUuid());
+
+        verify(repository, times(1)).findById(product.getUuid());
+        verify(repository, times(1)).delete(product);
     }
 
     @Test
-    void testFindAllProduct() {
+    @DisplayName("Product not found case")
+    void testDeleteProductCase2() {
 
+        when(repository.findById(product.getUuid())).thenReturn(Optional.empty());
+
+        NoSuchElementException exception = Assertions.assertThrows(NoSuchElementException.class,
+                () -> service.deleteProduct(product.getUuid()));
+
+        Assertions.assertEquals(exception, exception);
+
+        verify(repository, times(1)).findById(product.getUuid());
+    }
+
+    @Test
+    @DisplayName("Case confirm Deletion Product")
+    void testDeleteProductCase3() {
+
+        Optional<Product> productBefore = repository.findById(product.getUuid());
+        assertTrue(productBefore.isEmpty());
+
+        repository.delete(product);
+
+        Optional<Product> productAfter = repository.findById(product.getUuid());
+        assertFalse(productAfter.isPresent());
+    }
+
+    @Test
+    @DisplayName("Successful case returns all products")
+    void testFindAllProductCase1() {
+
+        when(repository.findAll()).thenReturn(List.of(product));
+
+        service.findAllProduct();
+
+        verify(repository).findAll();
+    }
+
+    @Test
+    @DisplayName("Case of returning an empty product list")
+    void testFindAllProductCase2() {
+        when(repository.findAll()).thenReturn(Collections.emptyList());
+
+        NotFoundException exception = Assertions.assertThrows(NotFoundException.class, () -> service.findAllProduct());
+
+        assertEquals("Products list not found", exception.getMessage());
+
+        verify(repository, times(1)).findAll();
     }
 
     @Test
